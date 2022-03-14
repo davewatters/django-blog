e0b2077ce92365eb77162a8f157778432d23852d
davewatters <89341297+davewatters@users.noreply.github.com>
2022-03-14 18:31:42 +0000

    add user story: #8,9,10 Manage Posts, create drafts, approve comments

--- blog/models.py
+++ blog/models.py
@@ -12,10 +12,10 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
@@ -41,7 +41,7 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["created_on"] 
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
