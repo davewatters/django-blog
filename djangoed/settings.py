9dd2029a6ebab761a8f7797070d8b3ad720b88bb
davewatters <89341297+davewatters@users.noreply.github.com>
2022-03-22 20:55:48 +0000

    refactor minor settings issue

--- djangoed/settings.py
+++ djangoed/settings.py
@@ -29,7 +29,7 @@ DEBUG = DEVELOPMENT
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 if DEVELOPMENT:
-    ALLOWED_HOSTS = [ 'localhost', '127.0.0.1' ]
+    ALLOWED_HOSTS = [ 'localhost' ]
 else:
     ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOSTS') ]
 
